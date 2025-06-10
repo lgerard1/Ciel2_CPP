c8b6fcaa9e30bea73e162acbed409b395cbcc0ca
lgerard1 <145434339+lgerard1@users.noreply.github.com>
2025-06-10 16:22:04 +0200

    Add files via upload

---
+++ TD1_TableauAdherents/public_html/js/listeAdherents.js
@@ -0,0 +1,34 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+
+$(document).ready(function ()
+{
+    $.getJSON('listeAdherents', function (adherents)
+    {
+        adherents.forEach(function (adherent) {
+            $("tbody").append(`
+<tr class="align-middle">
+<td class="text-center"><input type="checkbox" class="form-check-input"></td>
+<td>${adherent.nom}</td>
+<td>${adherent.prenom}</td>
+<td>${adherent.adresse}, ${adherent.codePostal} ${adherent.ville}</td>
+<td class="text-center">${adherent.telephone}</td>
+<td class="text-center">${adherent.mail}</td>
+<td class="text-center">
+<div class="btn-group">
+<button class="btn btn-light me-2">&#128394;</button>
+<button class="btn btn-light">&#128465;</button>
+</div>
+</td>
+</tr>
+`);
+        });
+    }).fail(function (jqxhr, textStatus, error)
+    {
+        const err = textStatus + ", " + error;
+        console.error("Erreur de chargement des adhérents: " + err);
+    });
+});
